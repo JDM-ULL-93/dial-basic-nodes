54577b334336e1ae2a9476023f0eaa5c4474f5fe
David Afonso <davafons@gmail.com>
2020-05-06 22:51:16 +0100

    :construction: Predefined models

--- dial_basic_nodes/predefined_models/predefined_models_widget.py
+++ dial_basic_nodes/predefined_models/predefined_models_widget.py
@@ -3,18 +3,28 @@
 from typing import Optional
 
 import dependency_injector.providers as providers
-from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QVBoxLayout, QWidget
 from tensorflow.keras.models import Model  # noqa: F401
+
+from .predefined_models_window import PredefinedModelsWindowFactory
 
 
 class PredefinedModelsWidget(QWidget):
-    def __init__(self, parent: "QWidget" = None):
+    def __init__(self, predefined_models_window, parent: "QWidget" = None):
         super().__init__(parent)
 
-        self._model: Optional["Model"] = None
+        self._predefined_models_window = predefined_models_window
+
+        self._main_layout = QVBoxLayout()
+        self._main_layout.setContentsMargins(0, 0, 0, 0)
+        self._main_layout.addWidget(predefined_models_window)
+
+        self.setLayout(self._main_layout)
 
     def get_model(self) -> Optional["Model"]:
-        return self._model
+        return self._predefined_models_window.selected_model()
 
 
-PredefinedModelsWidgetFactory = providers.Factory(PredefinedModelsWidget)
+PredefinedModelsWidgetFactory = providers.Factory(
+    PredefinedModelsWidget, predefined_models_window=PredefinedModelsWindowFactory
+)
