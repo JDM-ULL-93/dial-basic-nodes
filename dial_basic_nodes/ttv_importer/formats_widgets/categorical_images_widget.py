67aeb2fd2a575492a9c3f3ef3bc4d1659faa0ff4
David Afonso <davafons@gmail.com>
2020-05-10 22:05:35 +0100

    :sparkles: Glaucoma dataset imported successfully

--- dial_basic_nodes/ttv_importer/formats_widgets/categorical_images_widget.py
+++ dial_basic_nodes/ttv_importer/formats_widgets/categorical_images_widget.py
@@ -1,7 +1,26 @@
 # vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
 
+import re
+
 import dependency_injector.providers as providers
-from PySide2.QtWidgets import QLabel, QVBoxLayout, QWidget
+from dial_core.datasets import TTVSets
+from dial_core.datasets.datatype import DataType
+from dial_core.datasets.io import CategoricalImgDatasetIO
+from dial_core.utils import log
+from PySide2.QtWidgets import (
+    QComboBox,
+    QFormLayout,
+    QGroupBox,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
+    QWidget,
+)
+
+from .file_loader_group import FileLoaderGroup
+
+LOGGER = log.get_logger(__name__)
 
 
 class CategoricalImagesWidget(QWidget):
@@ -11,7 +30,93 @@ class CategoricalImagesWidget(QWidget):
         self._main_layout = QVBoxLayout()
         self._main_layout.addWidget(QLabel("Not Implemented"))
 
+        self._train_dir_loader = FileLoaderGroup("Train", pick_directories=True)
+        self._test_dir_loader = FileLoaderGroup("Test", pick_directories=True)
+        self._validation_dir_loader = FileLoaderGroup(
+            "Validation", pick_directories=True
+        )
+
+        self._organization_combobox = QComboBox()
+        self._organization_combobox.addItem(
+            "Extract Category from Folders",
+            CategoricalImgDatasetIO.Organization.CategoryOnFolders,
+        )
+        self._organization_combobox.addItem(
+            "Extract Category from Filename",
+            CategoricalImgDatasetIO.Organization.CategoryOnFilename,
+        )
+
+        self._category_regex_textbox = QLineEdit("(.*)")
+        self._category_regex_layout = QFormLayout()
+        self._category_regex_layout.addRow(
+            "Regex for extracting the category from filename",
+            self._category_regex_textbox,
+        )
+
+        self._regex_test_input_textbox = QLineEdit()
+        self._regex_test_output_textbox = QLineEdit()
+        self._regex_test_output_textbox.setReadOnly(True)
+
+        self._regex_test_layout = QHBoxLayout()
+        self._regex_test_layout.addWidget(QLabel("Test: (Input)"))
+        self._regex_test_layout.addWidget(self._regex_test_input_textbox)
+        self._regex_test_layout.addWidget(QLabel(" => Output: "))
+        self._regex_test_layout.addWidget(self._regex_test_output_textbox)
+
+        self._options_groupbox = QGroupBox()
+
+        self._main_layout = QVBoxLayout()
+        self._main_layout.setContentsMargins(0, 0, 0, 0)
+        self._main_layout.addWidget(self._train_dir_loader)
+        self._main_layout.addWidget(self._test_dir_loader)
+        self._main_layout.addWidget(self._validation_dir_loader)
+        self._main_layout.addWidget(self._organization_combobox)
+        self._main_layout.addLayout(self._category_regex_layout)
+        self._main_layout.addLayout(self._regex_test_layout)
+
         self.setLayout(self._main_layout)
+
+        self._category_regex_textbox.textChanged.connect(lambda: self._reload_regex())
+        self._regex_test_input_textbox.textChanged.connect(lambda: self._reload_regex())
+
+        self._reload_regex()
+
+    def load_ttv(self, name: str, x_type: DataType, y_type: DataType):
+        LOGGER.debug("Loading TTV %s", name)
+
+        def load_dataset(dataset_dir):
+            return (
+                # Subdir
+                # Organization
+                # Categories
+                CategoricalImgDatasetIO()
+                .set_x_type(x_type)
+                .set_y_type(y_type)
+                .set_organization(self._organization_combobox.currentData())
+                .set_filename_category_regex(self._category_regex_textbox.text())
+                .load(dataset_dir)  # Absolute path set on filename
+            )
+
+        train = load_dataset(self._train_dir_loader.path)
+        test = load_dataset(self._test_dir_loader.path)
+        validation = load_dataset(self._validation_dir_loader.path)
+
+        return TTVSets(name, train, test, validation)
+
+    def _reload_regex(self):
+        regex_string = self._category_regex_textbox.text()
+
+        input_text = self._regex_test_input_textbox.text()
+
+        match = re.match(regex_string, input_text)
+
+        if match:
+            try:
+                self._regex_test_output_textbox.setText(match.group(1))
+            except KeyError:
+                self._regex_test_output_textbox.setText(match.group(0))
+        else:
+            self._regex_test_output_textbox.setText("[No match]")
 
 
 CategoricalImagesWidgetFactory = providers.Factory(CategoricalImagesWidget)
--- dial_basic_nodes/ttv_importer/formats_widgets/npz_widget.py
+++ dial_basic_nodes/ttv_importer/formats_widgets/npz_widget.py
@@ -5,71 +5,13 @@ import os
 import dependency_injector.providers as providers
 from dial_core.datasets import TTVSets
 from dial_core.datasets.datatype import DataType
-from dial_core.datasets.io import NpzDatasetIO, TTVSetsIO
+from dial_core.datasets.io import NpzDatasetIO
 from dial_core.utils import log
-from PySide2.QtWidgets import (
-    QFileDialog,
-    QGroupBox,
-    QHBoxLayout,
-    QLineEdit,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-)
+from PySide2.QtWidgets import QVBoxLayout, QWidget
+
+from .file_loader_group import FileLoaderGroup
 
 LOGGER = log.get_logger(__name__)
-
-
-class FileLoaderGroup(QGroupBox):
-    def __init__(
-        self,
-        title: str,
-        parent: "QWidget" = None,
-        filter: str = "",
-        pick_directories: bool = False,
-    ):
-        super().__init__(title, parent)
-
-        self._file_picker_dialog = QFileDialog(
-            caption=f'Picking a "{title}" file...', filter=filter
-        )
-        self._file_picker_dialog.setFileMode(
-            QFileDialog.DirectoryOnly if pick_directories else QFileDialog.ExistingFile
-        )
-
-        self._pick_file_text = QLineEdit()
-        self._pick_file_text.setReadOnly(True)
-        self._pick_file_text.setPlaceholderText("Path...")
-        self._pick_file_button = QPushButton("Load")
-
-        self._pick_file_layout = QHBoxLayout()
-        self._pick_file_layout.addWidget(self._pick_file_text)
-        self._pick_file_layout.addWidget(self._pick_file_button)
-
-        self._main_layout = QVBoxLayout()
-        self._main_layout.addLayout(self._pick_file_layout)
-
-        self.setLayout(self._main_layout)
-
-        self._pick_file_button.clicked.connect(self._load_from_filesystem)
-
-    @property
-    def layout(self) -> "QVBoxLayout":
-        return self._main_layout
-
-    @property
-    def path(self) -> str:
-        return self._pick_file_text.text()
-
-    @path.setter
-    def path(self, path: str):
-        self._pick_file_text.setText(path)
-
-    def _load_from_filesystem(self):
-        LOGGER.debug("Picking file...")
-
-        if self._file_picker_dialog.exec():
-            self._pick_file_text.setText(self._file_picker_dialog.selectedFiles()[0])
 
 
 class NpzWidget(QWidget):
