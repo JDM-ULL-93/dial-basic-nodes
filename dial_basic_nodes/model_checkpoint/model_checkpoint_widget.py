be4e08f0501d4c12f09183c9a1373ec1a168bf8f
David Afonso <davafons@gmail.com>
2020-05-14 03:30:45 +0100

    :sparkles: More widgets for the model checkpoint

--- dial_basic_nodes/model_checkpoint/model_checkpoint_widget.py
+++ dial_basic_nodes/model_checkpoint/model_checkpoint_widget.py
@@ -5,7 +5,17 @@ from typing import List
 import dependency_injector.providers as providers
 from dial_core.utils import log
 from PySide2.QtCore import QSize
-from PySide2.QtWidgets import QFileDialog, QFormLayout, QLineEdit, QPushButton, QWidget
+from PySide2.QtWidgets import (
+    QCheckBox,
+    QFileDialog,
+    QFormLayout,
+    QGroupBox,
+    QHBoxLayout,
+    QLineEdit,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
 from tensorflow.keras.callbacks import Callback
 
 LOGGER = log.get_logger(__name__)
@@ -20,10 +30,25 @@ class ModelCheckpointWidget(QWidget):
         self._save_path_textbox.setPlaceholderText("Directory to save model files...")
         self._save_path_button = QPushButton("Select...")
         self._save_path_button.clicked.connect(self._pick_save_directory)
+        self._save_path_layout = QHBoxLayout()
+        self._save_path_layout.addWidget(self._save_path_textbox)
+        self._save_path_layout.addWidget(self._save_path_button)
+
+        self._filename_textbox = QLineEdit("model.{epoch:02d}--{val_loss:.2f}.h5")
+
+        self._weights_only_checkbox = QCheckBox("Weights only")
 
         # Layouts
-        self._main_layout = QFormLayout()
-        self._main_layout.addRow("Save directory:", self._save_path_textbox)
+        self._settings_layout = QFormLayout()
+        self._settings_layout.addRow("Save directory:", self._save_path_layout)
+        self._settings_layout.addRow("File name:", self._filename_textbox)
+        self._settings_layout.addRow("Save weights only?", self._weights_only_checkbox)
+
+        self._settings_group = QGroupBox("Model Checkpoint export settings")
+        self._settings_group.setLayout(self._settings_layout)
+
+        self._main_layout = QVBoxLayout()
+        self._main_layout.addWidget(self._settings_group)
 
         self.setLayout(self._main_layout)
 
@@ -39,7 +64,7 @@ class ModelCheckpointWidget(QWidget):
             LOGGER.debug("Picking save directory cancelled.")
 
     def sizeHint(self) -> "QSize":
-        return QSize(400, 200)
+        return QSize(500, 200)
 
     def __reduce__(self):
         return (ModelCheckpointWidget, (), super().__getstate__())
